40b7e0d3b5ef5b738e0e22355b2b94737658ba89
jlgerber <jlgerber@gmail.com>
2015-03-23 02:23:49 -0700

    updated tests as well as added webstorm .idea

--- test/query.test.js
+++ test/query.test.js
@@ -6,7 +6,7 @@ var query = require("../lib/query.js");
 var assert = require('assert');
 var _ = require("underscore");
 describe("query", function (){
-    it("should expose a Component class, a Statement class, and a Compound class, as well as Relation and Operator Enums", function(){
+    it("Should expose a Component class, a Statement class, and a Compound class, as well as Relation and Operator Enums", function(){
         assert(query.hasOwnProperty("Component"));
         assert(query.hasOwnProperty("Statement"));
         assert(query.hasOwnProperty("Compound"));
@@ -28,7 +28,7 @@ describe("query", function (){
             assert(_.isEqual(component,new_c));
         });
 
-        it("Should be able to convert to appropriate string format via the toString method for 'like' relations", function () {
+        it("Should be able to convert to appropriate string format via the 'toString' method for 'like' relations", function () {
             //noinspection JSCheckFunctionSignatures
             var component = query.Component.create("department", query.Relation.like, "model");
             var scomponent = component.toString();
@@ -36,12 +36,54 @@ describe("query", function (){
             assert(scomponent === "department:model");
         });
 
-        it("Should be able to convert to appropriate string format via the toString method for 'is' relations", function () {
+        it("Should be able to convert to appropriate string format via the 'toString' method for 'is' relations", function () {
             //noinspection JSCheckFunctionSignatures
             var component = query.Component.create("department", query.Relation.is, "model");
             var scomponent = component.toString();
             assert(scomponent === 'department:"model"');
 
         });
+    });
+    describe("Statement", function (){
+
+        it("Should provide a static convenience method which returns a new instance of Statement given a single component.", function (){
+            //noinspection JSCheckFunctionSignatures
+            var component = query.Component.create("department", query.Relation.like, "model");
+            var statement = Statement.create(component);
+            assert(statement instanceof query.Statement);
+        });
+
+        it("Should provide a static convenience method which returns a new instance of Statement given multiple components.", function (){
+            //noinspection JSCheckFunctionSignatures
+            var statement = Statement.create(
+                query.Component.create("department",query.Relation.is,"model"),
+                query.Component.create("name", query.Relation.like, "testcube"),
+                query.Component.create("subcontext", query.Relation.is,["hi","md","lo"])
+            );
+            assert(statement instanceof query.Statement);
+        });
+/*
+        it("Should be able to serialize/de-serialize and reconstitute itself via the 'fromOLArray' static method", function () {
+            //noinspection JSCheckFunctionSignatures
+            var component = query.Component.create("department", query.Relation.like, "model");
+            var new_c = query.Component.fromOL(JSON.parse(JSON.stringify(component)));
+            assert(_.isEqual(component,new_c));
+        });
+
+        it("Should be able to convert to appropriate string format via the 'toString' method for 'like' relations", function () {
+            //noinspection JSCheckFunctionSignatures
+            var component = query.Component.create("department", query.Relation.like, "model");
+            var scomponent = component.toString();
+            assert(typeof scomponent === 'string');
+            assert(scomponent === "department:model");
+        });
+
+        it("Should be able to convert to appropriate string format via the 'toString' method for 'is' relations", function () {
+            //noinspection JSCheckFunctionSignatures
+            var component = query.Component.create("department", query.Relation.is, "model");
+            var scomponent = component.toString();
+            assert(scomponent === 'department:"model"');
+
+        });*/
     });
 });
